7d60924ddb4c95013ead9a8fb1151dd84f02ed45
mh37 <78908148+mh37@users.noreply.github.com>
2022-02-10 23:24:42 +0200

    adding bootstra.min.css to booklist

--- src/main/java/com/example/mbookstore/model/BookRepository.java
+++ src/main/java/com/example/mbookstore/model/BookRepository.java
@@ -1,9 +1,11 @@
 package com.example.mbookstore.model;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface BookRepository extends CrudRepository <Book, Long>  {
 
 
