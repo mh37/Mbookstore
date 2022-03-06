4cae261e0da27ff570f18a669d56dad93953a501
mh37 <78908148+mh37@users.noreply.github.com>
2022-03-06 22:52:03 +0200

    Bookstore: Securing the web application

--- src/main/java/com/example/mbookstore/MbookstoreApplication.java
+++ src/main/java/com/example/mbookstore/MbookstoreApplication.java
@@ -1,9 +1,6 @@
 package com.example.mbookstore;
 
-import com.example.mbookstore.model.Book;
-import com.example.mbookstore.model.BookRepository;
-import com.example.mbookstore.model.Category;
-import com.example.mbookstore.model.CategoryRepository;
+import com.example.mbookstore.model.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.CommandLineRunner;
@@ -18,7 +15,7 @@ public class MbookstoreApplication{
 	}
 
 	@Bean
-	CommandLineRunner runner(BookRepository repository, CategoryRepository crepository) {
+	CommandLineRunner runner(BookRepository repository, CategoryRepository crepository, UserRepository urepository) {
 		return args -> {
 			crepository.save(new Category("Horror"));
 			crepository.save(new Category("Sci-Fi"));
@@ -33,7 +30,11 @@ public class MbookstoreApplication{
 			repository.save(new Book("Testers Tests","Frank Tester",2020,crepository.findByName("History").get(0),"971-3-17-148510-0",29.90,100));
 			repository.save(new Book("Test of all Tests","Peter Testerman",2021,crepository.findByName("Sci-Fi").get(0),"971-3-17-158511-0",25.95,40));
 
-
+			// Create users: admin/admin user/user
+			User user1 = new User("user", "$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6", "USER");
+			User user2 = new User("admin", "$2a$10$0MMwY.IQqpsVc1jC8u7IJ.2rT8b0Cd3b3sfIBGV2zfgnPGtT4r0.C", "ADMIN");
+			urepository.save(user1);
+			urepository.save(user2);
 
 			for (Book b : repository.findAll()) {
 				System.out.println(b.toString());
--- src/main/java/com/example/mbookstore/controller/BookController.java
+++ src/main/java/com/example/mbookstore/controller/BookController.java
@@ -5,6 +5,7 @@ import com.example.mbookstore.model.Book;
 import com.example.mbookstore.model.BookRepository;
 import com.example.mbookstore.model.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -13,13 +14,19 @@ import java.util.List;
 import java.util.Optional;
 
 
-
 @Controller
 public class BookController {
     @Autowired
     private BookRepository repository;
+
     @Autowired
     private CategoryRepository crepository;
+
+    // Handle Login
+    @RequestMapping(value="/login")
+    public String login() {
+        return "login";
+    }
 
     //Show a list of all books
     @RequestMapping("/booklist")
@@ -29,6 +36,7 @@ public class BookController {
     }
 
     //Delete a book based on the ID
+    @PreAuthorize("hasAuthority('ADMIN')")
     @GetMapping(value = "/deletebook/{id}")
     public String deleteBook(@PathVariable("id") Long id) {
         repository.deleteById(id);
@@ -78,21 +86,6 @@ public class BookController {
     @RequestMapping(value="/search/{title}", method = RequestMethod.GET)
     public @ResponseBody List<Book> findTitleRest(@PathVariable("title") String title) {
         return (List<Book>) repository.findByTitle(title);
-    }
-
-    //Delete a book based on the ID
-    @RequestMapping(value = "/delete/{id}", method = RequestMethod.DELETE)
-    public @ResponseBody String delete(@PathVariable("id") Long id) {
-        repository.deleteById(id);
-        return "{\"success\":\"true\"}";
-    }
-
-    //Adding a book
-    @RequestMapping(value = "/add", method = RequestMethod.PUT)
-    public @ResponseBody String add(Model model){
-        model.addAttribute("book", new Book());
-        model.addAttribute("categories", crepository.findAll());
-        return "{\"success\":\"true\"}";
     }
 
 
