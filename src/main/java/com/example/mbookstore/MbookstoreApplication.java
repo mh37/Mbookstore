9257f13e153f292209615853b8f3e8425a6c5041
mh37 <78908148+mh37@users.noreply.github.com>
2022-03-29 08:48:27 +0300

    remove default user creation

--- src/main/java/com/example/mbookstore/MbookstoreApplication.java
+++ src/main/java/com/example/mbookstore/MbookstoreApplication.java
@@ -31,9 +31,9 @@ public class MbookstoreApplication{
 			//repository.save(new Book("Test of all Tests","Peter Testerman",2021,crepository.findByName("Sci-Fi").get(0),"971-3-17-158511-0",25.95,40));
 
 			// Create users: admin/admin user/user
-			User user1 = new User("user", "$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6", "USER");
+			//User user1 = new User("user", "$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6", "USER");
 			User user2 = new User("admin", "$2a$10$0MMwY.IQqpsVc1jC8u7IJ.2rT8b0Cd3b3sfIBGV2zfgnPGtT4r0.C", "ADMIN");
-			urepository.save(user1);
+			//urepository.save(user1);
 			urepository.save(user2);
 
 			//for (Book b : repository.findAll()) {
